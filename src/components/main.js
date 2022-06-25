8ce402cc9f00b025619bb0f49b0a73c09b6f0503
unknown <luispomare92@gmail.com>
2022-06-25 09:37:01 -0500

    Create store to host the data and pass it to the main hook

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,16 @@
 import StaticData from './static-data';
 import DynamicData from './dynamic-data';
+import { useState } from 'react';
+import data from '../store.js';
 
-export default function main() {
+console.log(data);
+export default function Main() {
+  const [state, setState] = useState(data);
   return (
     <main>
       <StaticData />
       <DynamicData />
+      {state.ages[0]}
     </main>
   );
 }
