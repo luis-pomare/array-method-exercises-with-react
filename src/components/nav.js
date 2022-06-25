e000bd2c5fad6aabfe15f6fddbd1f7fdb37466f4
unknown <luispomare92@gmail.com>
2022-06-25 07:58:20 -0500

    Create nav component

---
+++ src/components/nav.js
@@ -0,0 +1,11 @@
+export default function nav() {
+  return (
+    <nav>
+      <ul>
+        <li>forEach();</li>
+        <li>filter();</li>
+        <li>map();</li>
+      </ul>
+    </nav>
+  );
+}
